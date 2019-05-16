0ca7d2767325716c85d972bbe4cf7671013ac8e1
Sarah <crab.apples00@gmail.com>
2019-05-16 13:50:37 -0700

    Transfer to lab

--- isa.py
+++ isa.py
@@ -4,6 +4,14 @@ Created on Wed May 15 22:15:43 2019
 
 @author: craba
 """
+import numpy as np
+def ISA(K, A, B, eps = 0.0):
+    V0 = K;
 
-def ISA(K, A, B):
-    return True
+        
+    return D
+
+def image(X, tol):
+    U,D,V = np.linalg.svd(X);
+    ind = np.argwhere(D < tol);
+    return U[0:ind]; # range of X where singular value is larger than tolerance        
