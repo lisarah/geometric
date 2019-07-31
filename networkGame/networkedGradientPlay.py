41dc2b5a0e5fd18c31569dfcf4bc3d841d9eea24
Sarah Li <crab.apples00@gmail.com>
2019-07-30 21:01:56 -0700

    working multiplayer general sum game

--- networkGame/networkedGradientPlay.py
+++ networkGame/networkedGradientPlay.py
@@ -8,6 +8,7 @@ Created on Fri Jul 26 16:39:01 2019
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import util as ut
 
 plt.close('all');
 n = 4;
@@ -27,22 +28,40 @@ adjacency = nx.to_numpy_matrix(smallWorld); # column of adjacency gives who colu
 N,N = adjacency.shape;
 
 Q = np.zeros((N*n, N*n));
-normSum = np.zeros((N));
+normSum = np.zeros((N)); maxEig = np.zeros((N)); minEig = np.zeros((N))
 for i in range(N):
-    Q[i*n:(i+1)*n,i*n:(i+1)*n] = np.multiply(np.random.rand(4,4),Qii);
+    diagNorm = np.multiply(np.random.rand(4,4),Qii);
+    Q[i*n:(i+1)*n, i*n:(i+1)*n] = diagNorm + diagNorm.T;
+    
+    w,v = np.linalg.eig(Q[i*n:(i+1)*n, i*n:(i+1)*n]);
+    Q[i*n:(i+1)*n, i*n:(i+1)*n] = Q[i*n:(i+1)*n, i*n:(i+1)*n]/2./(abs(w[0])+1e-4);
+    maxEig[i] = w[0]; minEig[i] = w[n-1];
+    
     neighbours, fet = np.where(adjacency[:,i] == 1);
     for neigh in neighbours: 
         Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n] = np.multiply(np.random.rand(4,4),Qneighbour);
-        normSum[i] += np.linalg.norm(Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n] , 2);
-    if np.linalg.norm(Q[i*n:(i+1)*n,i*n:(i+1)*n]) < normSum[i]:
-        Q[i*n:(i+1)*n,i*n:(i+1)*n] = 1.1*normSum[i] / np.linalg.norm(Q[i*n:(i+1)*n,i*n:(i+1)*n], 2)*Q[i*n:(i+1)*n,i*n:(i+1)*n];
-
+        normSum[i] += np.linalg.norm(Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n], 2);
+    for neigh in neighbours:
+        Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n] =  Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n]/normSum[i]/2.;
 
 Gamma = np.eye(N*n);
-for i in range(N):
-    gammai =2/(normSum[i] +np.linalg.norm(Q[i*n:(i+1)*n,i*n:(i+1)*n])) ;
-    Gamma[i*n: (i+1)*n]  = gammai*Gamma[i*n: (i+1)*n]; 
+# step sizes----uniform Version
+S = 0.5*(Q + Q.T);
+u, s, vt = np.linalg.svd(S);
+ut.truncate(s);
+alpha = np.min(s[np.nonzero(s)]); 
+beta = np.linalg.norm(Q,2);
+gammai = alpha/(beta*beta);
+Gamma = gammai*Gamma;
 barA = np.eye(N*n)  - Gamma.dot(Q); 
 w, v = np.linalg.eig(barA);
 print (w)
-print (np.linalg.norm(barA,2))
+
+# simulate the system
+T = int(1e3);
+x0 = np.random.rand((n*N));
+timeLine = np.linspace(1,T ,T)
+xTraj = ut.runGradient(0.99*barA, x0,int(T));
+plt.figure();
+plt.plot(timeLine, xTraj.T);
+plt.show();
---
+++ networkGame/util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 14:33:38 2019
+
+@author: sarahli
+"""
+import numpy as np
+
+def truncate(mat, eps = 1e-10):
+    M = mat.shape;
+    for i in range(M[0]): 
+        if mat[i] <= eps:
+            mat[i] = 0;
+
+def runGradient(A, x0, T = 1000):
+    n  = x0.shape;
+    xHist = np.zeros((n[0], T));
+    for t in range(T):
+        if t == 0:
+            xHist[:,0] = x0;
+        else:
+            xHist[:, t] = A.dot(xHist[:, t-1]);
+    return xHist;
+        
