e6ddf9f35423727c3174366e152fceedf7fa7e08
Sarah Li <crab.apples00@gmail.com>
2019-08-02 00:15:59 -0700

    Implemented Dijkstras, need to test code

--- ssp.py
+++ ssp.py
@@ -18,7 +18,7 @@ def ssp(sources, sinks, incidence, capacity, cost):
         s = np.where(bVec == 1)[0];
         t = np.where(bVec == -1)[0];
         resG = residualGraph(flowVec);
-        path = dijkstra(resG, s, t, cost);
+        path, minCost = dijkstra(resG, s, t, cost);
         augment(s,t,path, flowVec, B);
     
     return flowVec
@@ -33,15 +33,56 @@ def residualGraph(capacity, flowVec, incidence):
             resGraph[sink,source] += flowVec[i];
             
     return resGraph;
-      
+def selectMinDistance(queue, length):
+    minInd = queue[0]; 
+    minLen = length[queue[0]];
+    for nodeInd in queue:
+        if length[nodeInd] < minLen:
+            minInd = nodeInd;
+            minLen = length[nodeInd];
+    return minInd, minLen;
+
 def dijkstra(G,s,t, cost):
     V,E = G.shape;
     length = np.zeros((V));
-    visited =[True]*V;
+    visited =[False]*V;
+    prevNode = np.zeros((V));
     length[:] = np.nan;
-    length[s] = 0; v
-    while len( np.where(visited)) < V:
+    length[s] = 0;
+    prevNode[:] = np.nan;
+    prevNode[s] = s;
+    queue = [];
+    queue.append(s);
+    while len(queue) > 0:
+        nextNode, nextNodeDist = selectMinDistance(queue);
+        visited[nextNode] = True;
+        for n in G.neighbors(nextNode):
+            if length[n] > nextNodeDist + cost[nextNode, n]:
+                length[n] = nextNodeDist + cost[nextNode,n];
+                prevNode[n] = nextNode;
+            if visited[n] == False:
+                queue.append(n);
+   
+    path = [t];
+    
+    while path[-1] != s:
+        curNode = path[-1];
+        path.append(prevNode[curNode]);
+    
+    path.reverse();
+    return path, minCost;
+        
         
 def augment(s,t,path, flow, B):
     B = B -np.min([])
     print ("Nothing written for augemntation");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
