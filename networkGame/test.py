3b2b7b3bfe6faecff2dbbba779b3067451b76029
Sarah Li <crab.apples00@gmail.com>
2019-08-05 21:27:12 -0700

    Added the case where the graph could have negative links

--- networkGame/test.py
+++ networkGame/test.py
@@ -27,4 +27,51 @@ else:
 # if decouplable we do the next step: 
 F = dd.ddController(invariantSub, A, B);
 print (F)
-print (C.dot(A+B.dot(F)).dot(E))
+print (C.dot(A+B.dot(F)).dot(E));
+
+#----------- second test ----------------------#
+A = np.zeros((5,5));
+A[1,0] = 1;
+A[2,1] = 1; A[2,3] = 1;
+A[3,4] = 1;
+A[4,0] = 1; A[4,4] = 1;
+B = np.zeros((5,1)); B[1,0] = 1;
+E = np.zeros((5,1)); E[0,0] = 1;
+C = np.zeros((1,5)); C[0,2] = 1;
+
+invariantSub = isa.ISA(isa.ker(C), A, B)
+
+if (isa.contained(E, invariantSub)):
+    print ("disturbance decouplable");
+else:
+    print ("not disturbance decouplable");
+    
+
+# if decouplable we do the next step: 
+F = dd.ddController(invariantSub, A, B);
+print (F)
+print (C.dot(A+B.dot(F)).dot(E));
+
+#----------- third  test ----------------------#
+A = np.zeros((6,6));
+A[1,0] = 1;
+A[2,1] = 1; A[2,3] = 1; A[2,5] = 1;
+A[3,4] = 1;
+A[4,0] = 1; 
+A[5,0] = 1;
+B = np.zeros((6,1)); B[1,0] = 1;
+E = np.zeros((6,1)); E[0,0] = 1;
+C = np.zeros((1,6)); C[0,2] = 1;
+
+invariantSub = isa.ISA(isa.ker(C), A, B)
+
+if (isa.contained(E, invariantSub)):
+    print ("disturbance decouplable");
+else:
+    print ("not disturbance decouplable");
+    
+
+# if decouplable we do the next step: 
+F = dd.ddController(invariantSub, A, B);
+print (F)
+print (C.dot(A+B.dot(F)).dot(E));
