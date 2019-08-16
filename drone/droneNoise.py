ce13da0096010dc9c216782bcb2bfd68c0a2e1a2
Sarah Li <crab.apples00@gmail.com>
2019-08-16 12:51:36 -0700

    Added noisy drone code, started gossip example as well

--- drone/droneNoise.py
+++ drone/droneNoise.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 15 10:19:16 2019
+Created on Fri Aug 16 12:29:43 2019
 
 @author: craba
 """
@@ -42,49 +42,21 @@ def droneDyn(graph, couplingCoeff, N, n):
 # drone cost functions
 N = 4; # number of drones
 n = 2; # state space size of each drone
-T = 1000;
+T = 200;
 A = np.eye(n,n); 
-B = 0.24*np.eye(n);
 ref = np.zeros((n*N,T));
 ref = np.random.rand(n*N)*2.;
 
 
 noise = np.random.rand(n,T) - 0.5;
-#---------- game ------------------
+#---------- game graph ------------------
 completeGraph = np.ones((N,N)) - np.eye(N);
 uncoupledGraph = np.zeros((N,N));
 k4Graph= np.tril(np.ones((N,N)), k = -1);
-lineGraph = np.zeros((N,N)); lineGraph[1:4, 0:3] = np.eye(N-1);
-L, completeGamma = droneDyn(completeGraph,0.2, N, n);
+
+L, completeGamma = droneDyn(completeGraph,0.15, N, n);
 uncoupledL, uncoupledGamma = droneDyn(uncoupledGraph, 0.04, N,n);
-k4L, k4Gamma = droneDyn(k4Graph, 0.33, N,n);
-lineL, lineGamma = droneDyn(lineGraph, 0.4, N, n);
-#DW = np.eye(n*N);
-#uncoupleDW = np.eye(n*N);
-## Complete graph
-#for i in range(N):
-#    DW[n*i:n*(i+1), n*i:n*(i+1)] = (A + A.T); # 
-#    uncoupleDW[n*i:n*(i+1), n*i:n*(i+1)] = (A + A.T); # 
-##    print DW[i:2+i]
-#    for j in range(N):
-#        if j != i:            
-#            DW[n*i:n*(i+1), n*i:n*(i+1)] += -(B + B.T); 
-#            if j < i:
-#                DW[n*i:n*(i+1), n*j:n*(1+j)] = B+B.T;
-#                DW[n*j:n*(1+j), n*i:n*(i+1)] = (B + B.T);
-## step sizes
-#w, eigVecs = np.linalg.eig(0.25*(DW + DW.T).T.dot(DW + DW.T));
-#alpha = np.min(w);
-#w2, eigVecs = np.linalg.eig(DW.T.dot(DW));
-#beta = np.max(w);
-#gamma = np.eye(n*N)*alpha/beta; 
-#
-##gamma[0,0] = gamma[0,0] / 2;
-##gamma[1,1] = gamma[1,1] / 2;
-#L= np.eye(n*N) - gamma.dot(DW);
-#uncoupledL = np.eye(n*N) - gamma.dot(uncoupleDW);
-#wL, eigL = np.linalg.eig(L);
-#print (wL);
+k4L, k4Gamma = droneDyn(k4Graph, 0.15, N,n);
 
 diagA = np.zeros((n*N, n*N));
 for i in range(N):
@@ -93,31 +65,32 @@ for i in range(N):
 #--------------propogating dynamics -----------------#
 subStep  = 1;
 x = np.zeros((n*N,T*subStep),dtype=np.complex_);
-#noisyx = np.zeros((n*N, T*subStep),dtype=np.complex_);
-uncoupledX= np.zeros((n*N, T*subStep), dtype=np.complex_);
 k4x =  np.zeros((n*N, T*subStep), dtype=np.complex_);
+
+noisyx = np.zeros((n*N, T*subStep),dtype=np.complex_);
+uncoupledX= np.zeros((n*N, T*subStep), dtype=np.complex_);
 linex =  np.zeros((n*N, T*subStep), dtype=np.complex_);
+
 for t in range(T*subStep):
     if t == 0:
         x[:,0] = np.random.rand(n*N)*3;
-#        noisyx[:,0] = 1.0*x[:,0];
+        noisyx[:,0] = 1.0*x[:,0];
         uncoupledX[:,0] = 1.0*x[:,0];
         k4x[:,0] = 1.0*x[:,0];
-        linex[:, 0] = 1.0*x[:,0];
+
     else: 
         x[:,t] = L.dot(x[:,t-1]);
         uncoupledX[:,t] = uncoupledL.dot(uncoupledX[:, t-1]);
-#        noisyx[:,t] = L.dot(noisyx[:,t-1]);
         k4x[:,t] = k4L.dot(k4x[:,t-1]);
-        linex[:,t] = lineL.dot(linex[:,t-1])
-        c= completeGamma.dot(diagA).dot(ref);
-        x[:,t] += (c);
-#        noisyx[:,t] += (c);
-        uncoupledX[:,t] += uncoupledGamma.dot(diagA).dot(ref);
+        noisyx[:,t] = k4L.dot(noisyx[:,t-1]);
+ 
+        x[:,t] += completeGamma.dot(diagA).dot(ref);
         k4x[:,t] += k4Gamma.dot(diagA).dot(ref);
-        linex[:,t] += lineGamma.dot(diagA).dot(ref);
+        noisyx[:,t] += k4Gamma.dot(diagA).dot(ref);
+        uncoupledX[:,t] += uncoupledGamma.dot(diagA).dot(ref);
+
     # noise
-#    noisyx[4:4+n, t] += noise[:,t];
+    noisyx[4:4+n, t] += noise[:,t];
 plt.figure();
 ax = plt.axes(projection='3d')
 # Data for a three-dimensional line
@@ -125,10 +98,11 @@ zline = np.linspace(0, T*subStep, T*subStep, endpoint = False)
 
 for i in range(N):
 #    ax.scatter(uncoupledX[i*n,:].real,  uncoupledX[n*i+1,:].real, zline, label = 'uncoupled '+str(i+1));
-#    ax.plot3D(k4x[i*n,:].real, k4x[i*n+1,:].real, zline, '--', label = 'k4 '+str(i+1));
-    ax.plot3D(x[i*n,:].real,  x[n*i+1,:].real, zline, label = 'complete '+str(i+1));
-#    ax.plot3D(linex[i*n,:].real,  linex[n*i+1,:].real, zline, ':',label = 'line '+str(i+1));
-#    ax.plot3D(noisyx[i*n,:].real,  noisyx[n*i+1,:].real, zline, '--',label =str(i+1));
+    ax.scatter(k4x[i*n,:].real, k4x[i*n+1,:].real, zline, '--', label = 'k4 '+str(i+1));
+
+#    ax.plot3D(x[i*n,:].real,  x[n*i+1,:].real, zline, label = 'complete '+str(i+1));
+
+    ax.plot3D(noisyx[i*n,:].real,  noisyx[n*i+1,:].real, zline, '--',label ='noisy '+str(i+1));
 #    print(n*i, "   ", n*i+1)
 plt.legend();
 plt.show();
---
+++ drone/gossipNoise.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 12:47:17 2019
+
+@author: craba
+"""
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import util as ut
+import isa as isa
+import decouplingController as dd
+plt.close('all');
+n = 4;
+Qii = np.array([[0, 1, 0, 0], [1, 0, 0, 1], [1, 1, 0, 0], [0, 0, 1, 0]]);
+Qneighbour = np.zeros((4,4)); Qneighbour[0:2, 0:2] = np.eye(2);
+
+N = 3;
+smallWorld = nx.navigable_small_world_graph(N);
+circleSmallWorld = nx.circular_layout(smallWorld);
+plt.figure();
+nx.draw(smallWorld, circleSmallWorld, node_color=range(N*N), node_size=800, cmap=plt.cm.Blues)
+nx.draw_networkx_labels(smallWorld,circleSmallWorld,font_size=20,font_family='sans-serif')
+plt.show();
+
+adjacency = nx.to_numpy_matrix(smallWorld); # column of adjacency gives who column i receives
+
+N,N = adjacency.shape;
+
+Q = np.zeros((N*n, N*n));
+normSum = np.zeros((N)); maxEig = np.zeros((N)); minEig = np.zeros((N))
+for i in range(N):
+    diagNorm = np.multiply(np.random.rand(4,4),Qii);
+    Q[i*n:(i+1)*n, i*n:(i+1)*n] = diagNorm + diagNorm.T;
+    
+    w,v = np.linalg.eig(Q[i*n:(i+1)*n, i*n:(i+1)*n]);
+    Q[i*n:(i+1)*n, i*n:(i+1)*n] = Q[i*n:(i+1)*n, i*n:(i+1)*n]/2./(abs(w[0])+1e-4);
+    maxEig[i] = w[0]; minEig[i] = w[n-1];
+    
+    neighbours, fet = np.where(adjacency[:,i] == 1);
+    for neigh in neighbours: 
+        Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n] = np.multiply(np.random.rand(4,4),Qneighbour);
+        normSum[i] += np.linalg.norm(Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n], 2);
+    for neigh in neighbours:
+        Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n] =  Q[i*n:(i+1)*n, neigh*n:(neigh+1)*n]/normSum[i]/2.;
+
+Gamma = np.eye(N*n);
+# step sizes----uniform Version
+S = 0.5*(Q + Q.T);
+u, s, vt = np.linalg.svd(S);
+ut.truncate(s);
+alpha = np.min(s[np.nonzero(s)]); 
+beta = np.linalg.norm(Q,2);
+gammai = alpha/(beta*beta);
+Gamma = gammai*Gamma;
+barA = np.eye(N*n)  - Gamma.dot(Q); 
+w, v = np.linalg.eig(barA);
+print (w)
+
+# simulate the system
+T = int(1e3);
+x0 = np.random.rand((n*N));
+timeLine = np.linspace(1,T ,T)
+xTraj = ut.runGradient(0.99*barA, x0,T=int(T));
+plt.figure();
+plt.title("no noise, check if these step sizes converges")
+plt.plot(timeLine, xTraj.T);
+plt.show();
+
+#------------------ DISTURBANCE -------------------
+E  = np.array([1,0,0,0]);
+C = np.array([0,0,0,1]);
+B = np.array([0,0,1,0]);
+
+barE = np.zeros((n*N, N)); barC = np.zeros((N,n*N)); barB = np.zeros((n*N,N))
+for i in range(N):
+    barE[n*i:(i+1)*n, i] = E;
+    barC[i, n*i:(i+1)*n] = C;
+    barB[n*i:(i+1)*n, i] =  B;
+invariantSub = isa.ISA(isa.ker(barC), barA, barB)
+
+if (isa.contained(barE, invariantSub)):
+    print ("disturbance decouplable");
+else:
+    print ("not disturbance decouplable");
+    
+    
+# if decouplable we do the next step: 
+F = dd.ddController(invariantSub, barA, barB);
+#print (F)
+#print (barC.dot(barA+barB.dot(F)).dot(barE))
+
+# -------------------simulate the system again-------------------
+T = int(1e3);
+#x0 = np.random.rand((n*N));
+timeLine = np.linspace(1,T ,T)
+xTraj = ut.runGradient((0.99*barA + barB.dot(F)), x0, T= int(T));
+plt.figure();
+plt.title("simulated the disturbance decoupled closed loop without noise")
+plt.plot(timeLine, xTraj.T);
+plt.show();
+
+
+# if decouplable we do the next step: 
+F = dd.ddController(invariantSub, barA, barB);
+#print (F)
+#print (barC.dot(barA+barB.dot(F)).dot(barE))
+
+##---------- simulate noisy version -----------------------
+T = int(1e3);
+
+timeLine = np.linspace(1,T ,T);
+w = np.random.rand(9,T);
+
+xTraj = ut.runGradient((0.99*barA + barB.dot(F)), x0, barE.dot(w), int(T));
+plt.figure();
+plt.title("simulate the DD closed loop WIth Noise");
+plt.plot(timeLine, xTraj.T );
+plt.show();
+
+##---------- simulate noisy version without feedback control -----------------------
+T = int(1e3);
+
+timeLine = np.linspace(1,T ,T);
+#w = np.random.rand(9,T);
+
+xTraj = ut.runGradient((0.99*barA), x0, noise = barE.dot(w), T = int(T));
+plt.figure();
+plt.title("simulate the not DDed with NOISE")
+plt.plot(timeLine, xTraj.T );
+plt.show();
