44f97aa5e06f6bea21ac62aedee90659f219402e
lisarah <crab.apples00@gmail.com>
2019-09-26 10:20:34 +0200

    acc figure generation in pdf_* files

--- networkGame/pdf_IncreasingNoise.py
+++ networkGame/pdf_IncreasingNoise.py
@@ -10,6 +10,8 @@ for both linear and nonlinear dynamics
 import numpy as np
 import util as ut
 import matplotlib.pyplot as plt
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 plt.close('all');
 N = 4;
 J = np.eye(8)*2;
@@ -26,8 +28,7 @@ c = np.array([1,1, 1,-1, -1,-1, -1,1])*gamma;
 T = 1000;
 x0 = np.random.rand(2*N);
 linear = ut.runGradient(np.eye(2*N) - gamma*J, x0 = x0, T = T, offset=c);
-ut.plot2D(linear);
-
+#ut.plot2D(linear);
 
 # generate noise NE -- linear dynamics
 # input noise is at player 1
@@ -52,25 +53,25 @@ for sample in range(samples):
         
 
 ut.plot2D(diff/T, epsilon);
-plt.xlabel(r'$\epsilon$', fontsize = 15);
+plt.xlabel(r'$\epsilon$', fontsize = 18);
 plt.yscale('log');
-plt.ylabel(r'$\|\| y_i - x_i \|\|_2$', fontsize = 15);
+plt.ylabel(r'$\| y_i - x_i \|_2$', fontsize = 18);
 
 #----------- non linear version of everything that happened just now -------#
 def nonLinearGrad(xt, eps):
     Df = np.zeros((8));
     Df[0:2] = 2*(xt[0:2] - c[0:2]);
     
-    Df[2:4] = 2*(xt[2:4] - c[2:4]) + 1.5*xt[0:2] + eps*0.05*xt[0:2].dot(xt[0:2])*xt[0:2];
+    Df[2:4] = 2*(xt[2:4] - c[2:4]) + 1.5*xt[0:2] + eps*xt[0:2].dot(xt[0:2])*xt[0:2];
     
-    Df[4:6] = 2*(xt[4:6] - c[4:6]) + 1.5*xt[0:2] + eps*0.05*xt[0:2].dot(xt[0:2])*xt[0:2];
-    Df[6:8] = 2*(xt[6:8] - c[6:8]) + 0.5*(xt[2:4] - xt[4:6]) + eps*0.1*(xt[2:4].dot(xt[2:4]) - xt[4:6].dot(xt[4:6]));
+    Df[4:6] = 2*(xt[4:6] - c[4:6]) + 1.5*xt[0:2] + eps*xt[0:2].dot(xt[0:2])*xt[0:2];
+    Df[6:8] = 2*(xt[6:8] - c[6:8]) + 0.5*(xt[2:4] - xt[4:6]); # + eps*0.05*(xt[0:2].dot(xt[0:2]) - xt[0:2].dot(xt[0:2]));
     return xt - gamma*Df;
 
 # generate NE
 x0 = np.random.rand(2*N);
 nonLinear = ut.runNonlinearGradient(nonLinearGrad, x0 = x0, T = T);
-ut.plot2D(nonLinear);
+#ut.plot2D(nonLinear);
 #--------------------- noisy version ------------------------------#
 noise = np.zeros((N*2,T));
 samples = 100;
@@ -92,5 +93,5 @@ for sample in range(samples):
 
 ut.plot2D(diff/T, epsilon);
 plt.yscale('log');
-plt.xlabel(r'$\epsilon$', fontsize = 15);
-plt.ylabel(r'$\|\| y_i - x_i \|\|_2$', fontsize = 15);
+plt.xlabel(r'$\epsilon$', fontsize = 18);
+plt.ylabel(r'$\| y_i - x_i \|_2$', fontsize = 18);
--- networkGame/pdf_nonLinear.py
+++ networkGame/pdf_nonLinear.py
@@ -8,7 +8,7 @@ Increasing non linearity with fixed noise
 import numpy as np
 import util as ut
 import matplotlib.pyplot as plt
-plt.close('all');
+#plt.close('all');
 N = 4;
 
 J = np.eye(8)*2;
@@ -25,17 +25,17 @@ def nonLinearGrad(xt, eps):
     Df = np.zeros((8));
     Df[0:2] = 2*(xt[0:2] - c[0:2]);
     
-    Df[2:4] = 2*(xt[2:4] - c[2:4]) + 1.5*xt[0:2] + eps*0.05*xt[0:2].dot(xt[0:2])*xt[0:2];
+    Df[2:4] = 2*(xt[2:4] - c[2:4]) + 1.5*xt[0:2] + eps*xt[0:2].dot(xt[0:2])*xt[0:2];
     
-    Df[4:6] = 2*(xt[4:6] - c[4:6]) + 1.5*xt[0:2] + eps*0.05*xt[0:2].dot(xt[0:2])*xt[0:2];
-    Df[6:8] = 2*(xt[6:8] - c[6:8]) + 0.5*(xt[2:4] - xt[4:6]) + eps*0.1*(xt[2:4].dot(xt[2:4]) - xt[4:6].dot(xt[4:6]));
+    Df[4:6] = 2*(xt[4:6] - c[4:6]) + 1.5*xt[0:2] + eps*xt[0:2].dot(xt[0:2])*xt[0:2];
+    Df[6:8] = 2*(xt[6:8] - c[6:8]) + 0.5*(xt[2:4] - xt[4:6]); # + eps*0.05*(xt[2:4].dot(xt[2:4]) - xt[4:6].dot(xt[4:6]));
     return xt - gamma*Df;
 
 # generate NE in the noiseless case
 T = 1000;
 x0 = np.random.rand(2*N);
-nonLinear = ut.runNonlinearGradient(nonLinearGrad, x0 = x0, T = T);
-ut.plot2D(nonLinear);
+nonLinear = ut.runNonlinearGradient(nonLinearGrad, x0 = x0, eps = 0,T = T);
+#ut.plot2D(nonLinear);
 
 #--------------------- noisy version ------------------------------#
 noise = np.zeros((N*2,T));
@@ -45,10 +45,10 @@ diff = np.zeros((N*2, samples));
 epsilon = np.zeros(samples);
 for sample in range(samples):
     epsilon[sample] = sample*0.1;
-    nonLinear = ut.runNonlinearGradient(nonLinearGrad, 
-                                        x0 = x0, 
-                                        eps = epsilon[sample],
-                                        T = T);
+#    nonLinear = ut.runNonlinearGradient(nonLinearGrad, 
+#                                        x0 = x0, 
+#                                        eps = epsilon[sample],
+#                                        T = T);
     nonLinearNoisy = ut.runNonlinearGradient(nonLinearGrad,  
                                  x0 = x0, 
                                  noise = noise,
@@ -60,5 +60,5 @@ for sample in range(samples):
 
 ut.plot2D(diff/T, epsilon);
 plt.yscale('log');
-plt.xlabel(r'$\epsilon$', fontsize = 15);
-plt.ylabel(r'$\|\| y_i - x_i \|\|_2$', fontsize = 15);
+plt.xlabel(r'$\epsilon$', fontsize = 18);
+plt.ylabel(r'$\| y_i - x_i \|_2$', fontsize = 18);
